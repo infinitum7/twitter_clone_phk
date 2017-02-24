1011127828db20aa952eca94ba4571673f7c4bcb
Peter Kwag <peter.kwag@live.com>
2017-02-24 07:51:26 +0000

    Settings

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,6 @@ module SampleApp
     # Include the authenticity token in remote forms.
     config.action_view.embed_authenticity_token_in_remote_forms = true
     config.assets.paths << "#{Rails.root}/app/assets/videos"
+
   end
 end
